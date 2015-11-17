5900911f55c55e29397ab32109eb7a9494f710c8
Yaroslav Kynyk <Yaroslav_Kynyk@epam.com>
2015-11-17 15:38:16 +0100

    added test to ariphmetical operations

--- src/test/java/exercises/calculator/CalculatorTest.java
+++ src/test/java/exercises/calculator/CalculatorTest.java
@@ -19,4 +19,25 @@ public class CalculatorTest {
         String actual = Calculator.calculate("54+7-");
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    public void testDivide() {
+        String expected = "2";
+        String actual = Calculator.calculate("42/");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testMultiply() {
+        String expected = "8";
+        String actual = Calculator.calculate("24*");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testComplexExpression() {
+        String expected = "9";
+        String actual = Calculator.calculate("842*8/+");
+        assertThat(actual).isEqualTo(expected);
+    }
 }
